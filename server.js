4189b74a238783275ef068f560171267151d733a
Vedanshika <139014095+Vedanshika@users.noreply.github.com>
2023-07-13 01:50:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,37 +6,36 @@ const app = express();
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 // Set up static file serving
-app.use('/Equivalent_Messages/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Admin page route
-app.get('/Equivalent_Messages/admin', (req, res) => {
+app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
 // File upload endpoint for admin - MT Messages
-app.post('/Equivalent_Messages/upload-mt', upload.single('mtFile'), (req, res) => {
+app.post('/upload-mt', upload.single('mtFile'), (req, res) => {
   res.json({ message: 'MT file uploaded successfully!' });
 });
 
 // File upload endpoint for admin - MX Messages
-app.post('/Equivalent_Messages/upload-mx', upload.single('mxFile'), (req, res) => {
+app.post('/upload-mx', upload.single('mxFile'), (req, res) => {
   res.json({ message: 'MX file uploaded successfully!' });
 });
 
 // User page route
-app.get('/Equivalent_Messages/user', (req, res) => {
+app.get('/user', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'user.html'));
 });
 
 // API endpoint to retrieve MT messages
-app.get('/Equivalent_Messages/api/mt-messages', (req, res) => {
+app.get('/api/mt-messages', (req, res) => {
   const mtMessages = getFilesFromDirectory('uploads', '.txt');
   res.json(mtMessages);
 });
 
 // API endpoint to retrieve MX messages
-app.get('/Equivalent_Messages/api/mx-messages', (req, res) => {
+app.get('/api/mx-messages', (req, res) => {
   const mxMessages = getFilesFromDirectory('uploads', '.xml');
   res.json(mxMessages);
 });
@@ -48,7 +47,7 @@ function getFilesFromDirectory(directory, extension) {
   const filteredFiles = files.filter(file => path.extname(file).toLowerCase() === extension);
   const fileURLs = filteredFiles.map(file => ({
     fileName: file,
-    fileURL: `/Equivalent_Messages/uploads/${file}`
+    fileURL: `/uploads/${file}`
   }));
   return fileURLs;
 }
